a30b899c8b41f6e5a4842d766ae106c5c63aa6fa
Andrei Lyshchik <alyshchik@fastmail.com>
2025-08-23 20:53:30 +0300

    implement array initializers, local/file scoped/static

--- app/src/test/resources/programs/arrays.c
+++ app/src/test/resources/programs/arrays.c
@@ -73,5 +73,50 @@ int main(void) {
         return 7;
     }
 
+    int file_scope_sum = 0;
+    for (int i = 0; i < 3; i++) {
+        file_scope_sum += file_scope[i];
+    }
+    if (file_scope_sum != 15) {
+        return 2;
+    }
+    int file_scope_zero_padded_sum = 0;
+    for (int i = 0; i < 15; i++) {
+        file_scope_zero_padded_sum += file_scope_zero_padded[i];
+    }
+    if (file_scope_zero_padded_sum != 114) {
+        return 3;
+    }
+
+    int uninitialized[5];
+    for (int i = 0; i < 5; i++) {
+        uninitialized[i] = i + 1;
+    }
+    int uninitialized_sum = 0;
+    for (int i = 0; i < 5; i++) {
+        uninitialized_sum += uninitialized[i];
+    }
+    if (uninitialized_sum != 15) {
+        return 4;
+    }
+
+    int not_all_initialized[5] = { 4 + 8, 2 * 6 + a[1] };
+    int not_all_initialized_sum = 0;
+    for (int i = 0; i < 5; i++) {
+        not_all_initialized_sum += not_all_initialized[i];
+    }
+    if (not_all_initialized_sum != 25) {
+        return 5;
+    }
+
+    int static_array_sum = inc_static_array();
+    if (static_array_sum != 15) {
+        return 6;
+    }
+    int static_array_sum2 = inc_static_array();
+    if (static_array_sum2 != 20) {
+        return 7;
+    }
+
     return 0;
 }
